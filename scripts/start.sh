a428bd0b308786b05cf0010318e6cb1e674cfb72
Taz Hate <taz.inside@gmail.com>
2019-06-28 18:20:09 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-if [ -z "$CONFIG_FILE" ]
-then
-    /app/mikrotik-exporter -device $DEVICE -address $ADDRESS -user $USER -password $PASSWORD
-else
-    /app/mikrotik-exporter -config-file $CONFIG_FILE
-fi
+/app/mikrotik-exporter -device $DEVICE -address $ADDRESS -user $USER -password $PASSWORD -with-dhcp -with-pools -with-routes
